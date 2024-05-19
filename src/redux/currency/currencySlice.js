6f7358e499ecc1083138bb2d9a803e5f554e5998
Summershinne <145506570+Summershinne@users.noreply.github.com>
2024-05-19 12:03:11 +0200

    page3

--- src/redux/currency/currencySlice.js
+++ src/redux/currency/currencySlice.js
@@ -1,7 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { fetchBaseCurrency } from '../../redux/currency/operations';
+import { fetchBaseCurrency, fetchExchangeCurrency } from '../../redux/currency/operations';
 
-const initialState = { baseCurrency: '' };
+const initialState = {
+  baseCurrency: '',
+  exchangeInfo: null,
+  loading: false,
+  error: null,
+};
 const currencySlice = createSlice({
   name: 'currency',
   initialState,
@@ -13,7 +18,21 @@ const currencySlice = createSlice({
   extraReducers: builder =>
     builder.addCase(fetchBaseCurrency.fulfilled, (state, { payload }) => {
       state.baseCurrency = payload;
-    }),
+    })
+      .addCase(fetchExchangeCurrency.pending, (state) =>
+      {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchExchangeCurrency.fulfilled, (state, { payload }) => {
+        state.exchangeInfo = payload;
+        state.loading = false;
+      })
+      .addCase(fetchExchangeCurrency.rejected, (state, { payload }) => {
+        state.error = payload;
+        state.exchangeInfo = null;
+        state.loading = false;
+  })
 });
 
 export const { setBaseCurrency } = currencySlice.actions;
